156f84c11ab73d20c2c79e2b5e64402a910162eb
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2023-12-20 16:57:59 -0500

    on

---
+++ SB-MeanyBird/Assets/Scripts/Spawner.cs
@@ -0,0 +1,6 @@
+﻿//object that we will attach to the script for spawning object
+[Header("Spikes Object for contolling the game")]
+public GameObject spikes
+//height position of the spikes
+[Header("Default Height")]
+public float height;
