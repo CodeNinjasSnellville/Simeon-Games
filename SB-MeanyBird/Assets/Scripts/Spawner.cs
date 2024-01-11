22eebd4bd18cc21a02f494544670f2180efe84a9
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-01-11 18:05:45 -0500

    uj

--- SB-MeanyBird/Assets/Scripts/Spawner.cs
+++ SB-MeanyBird/Assets/Scripts/Spawner.cs
@@ -4,3 +4,24 @@ public GameObject spikes
 //height position of the spikes
 [Header("Default Height")]
 public float height;
+
+//Start is called bafore the first frame update
+void Start()
+{
+    //Start function repeating every 4 seconds
+    InvokeRepeating("InstantiateObjects", 1f, 4f);
+}
+
+//Update is called once per frame
+void Update()
+{
+    //Position for the gameobjects
+    transform.position = new Vector3(5, Random.Range(-height, height), 0);
+}
+
+//InstantiateObjects Function
+void InstantiateObjects()
+{
+    //Spawn object ny postion and rotation
+    InstantiateObjects(spikes, transform.position, transform,rotation)
+}
