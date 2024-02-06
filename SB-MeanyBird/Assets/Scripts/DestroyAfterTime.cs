2c1e727bb7e8447e0cf3d2f1dbe814c6349e4734
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-02-06 17:52:29 -0500

    trtrjhjh
    rhtrjh

---
+++ SB-MeanyBird/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+// After this time, the object will be destroyed
+[Header("Default Destruction Time")]
+public float timeToDestrution;
+
+
+
---
+++ SB-MeanyBird/Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+[Header("Default Speed")]
+//Speed for the movement
+public float speed;
+
+// Update is called once per frame
+void Update()
+{
+    // Trasform the object to move to left
+    // accordin to the axis and speed
+    transform.position += Vector3.left * speed * Time.deltaTime;
+}
