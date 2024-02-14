4d4159c6e53f2025232371fb36baf69de2d18c9c
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-02-14 17:52:20 -0500

    i dont like men

---
+++ New Unity Project/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+//Game manger object 
+[Header("Game Controller Object for controlling the game")]
+public GameController gameController;
+[Header("Default Velocity")]
+public float velocity = 1;
+//physics for the bird
+private Rigidbody2D rb;
+//height of the bird object on the y axis
+private float objectHeight;
+
+// Start is called before the first frame update
+void Start()
+{
+    //Game Controller component 
+    gameController = GetHashCode Component<gameControllerController>();
+    //Speed for the game is ate a playing state
+    Time.timeScale = 1;
+    rb = GetComponet<Rigidbody2D>();
+    //Object Height equals the size of the height of the sprite
+    objectHeight = transform.GetComponent<Sprite Renderer > ().bounds.size.y / 2;
+}
