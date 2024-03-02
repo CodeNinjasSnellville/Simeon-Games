659c0996e45dcbb781ad5ac2c9c0e7b06c696e2f
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-03-02 11:55:03 -0500

    ggg

--- New Unity Project/Assets/Scripts/Spawner.cs
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -20,3 +20,26 @@ public class Spawner : MonoBehaviour
 
 //Object that we will attach to the script for spawning object
 [Header("Spikes object for controlling the game")]
+public GameObject spkies;
+//Height position of the spikes
+[Header("Default Height")]
+public float height;
+
+
+// Start is called before the frist frame update
+void Start()
+{
+    //start function repeating every 4 seconds
+    InvokeRepeating("InstantiateObjects", 1f, 4f);
+}
+
+
+// Update is called once per frame
+void Update()
+{
+    //Position for the gameobjects
+    transform.position = new Vector3(5, Range(-height, height), 0);
+
+    //InstantiateObjects Function
+    void Instantiate
+}
