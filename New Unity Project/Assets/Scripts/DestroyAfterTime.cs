42e677c8ed1b6939d464ecfdc2f7be9ee1961844
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-03-21 17:53:42 -0400

    your mom

--- New Unity Project/Assets/Scripts/DestroyAfterTime.cs
+++ New Unity Project/Assets/Scripts/DestroyAfterTime.cs
@@ -17,3 +17,21 @@ public class DestroyAfterTime : MonoBehaviour
     }
 }
 
+
+//After this time, the object will be destroyed
+[Header("Default Destruction Time")]
+public float timeToDestruction;
+
+//Start is called before the first frame update
+void Start()
+{
+    //Execute DestroyObject function based on timeToDestruction
+    Invoke("DestroyObject", timeToDestruction);
+}
+
+// This function will destroy this object
+void DestoryObject()
+{
+    //Destroy object
+    DestroyAfterTime(gameObject);
+}
