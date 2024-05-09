3fed04dd360afd5df8dccf3222a5500f1888fea5
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-05-09 17:17:59 -0400

    asdfgsed

---
+++ SB sketch head/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform
+    //Defaut position for platform
+    float pos = 0;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //Interger i equals 1000
+        for (int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnPlatforms();
+        }
+    }
+
+    //Spawn platforms function
+    void SpawnPlatforms()
+    {
+        //Spawn platforms randomly on the x axis and place them on the Y axis every 2.5
+        Instantiate(platform, new Vector)
+    }
+}
---
+++ SB sketch head/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Rigibody2D object that is stored
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    [Header("Defalut Down Speed")]
+    //downward speed of the object
+    public float downSpeed = 20f;
+    [Header("Default Down Speed")]
+    //movement speed of the object
+    public float movementSpeed = 10f;
+    [Header("Default Directional Movement Speed")]
+    //movement direction of the object
+    public float movement = 0f;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        //variable equals to componet Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();
+        //movement equals Horizontal movement
+        //multiplied by the movement speed
+       
+    }
+
+    
+        //Fixedupdate called every fixed frame-rate frame.
+        void FixedUpdate()
+        {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        //if direction on x aixs is les than 0
+        if (movement < 0)
+
+            //object faces to the left
+            this.GetComponent<SpriteRenderer>().flipX = false;
+
+        //if diredtion on x axis is greater than 0
+        else
+        {
+            //object faces to the right
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }  
+
+}
+
