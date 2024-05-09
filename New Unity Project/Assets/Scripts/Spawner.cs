846a938946e4b04c8ca6c151531fea73431e08e9
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2024-05-09 17:18:32 -0400

    Update Spawner.cs

--- New Unity Project/Assets/Scripts/Spawner.cs
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -4,42 +4,39 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //InstantiateObjects Function
+    void Instantiate()
     {
 
     }
+    //Object that we will attach to the script for spawning object
+    [Header("Spikes object for controlling the game")]
+    public GameObject spkies;
+    //Height position of the spikes
+    [Header("Default Height")]
+    public float height;
+
+
+    // Start is called before the frist frame update
+    void Start()
+    {
+        //start function repeating every 4 seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        //Position for the gameobjects
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
 
     }
-}
-
-
-//Object that we will attach to the script for spawning object
-[Header("Spikes object for controlling the game")]
-public GameObject spkies;
-//Height position of the spikes
-[Header("Default Height")]
-public float height;
-
-
-// Start is called before the frist frame update
-void Start()
+//InstantiateObjects Function
+void InstantiateObjects()
 {
-    //start function repeating every 4 seconds
-    InvokeRepeating("InstantiateObjects", 1f, 4f);
+        //Spawn object by postion and rotation
+        Instantiate(spikes, transform.position, transform.rotation);
 }
-
-
-// Update is called once per frame
-void Update()
-{
-    //Position for the gameobjects
-    transform.position = new Vector3(5, Range(-height, height), 0);
-
-    //InstantiateObjects Function
-    void Instantiate
 }
